4a9f386cbc2240ffc38c2e6729cf3914bf79adcd
Joe Einertson <jeinerts@umm>
2017-09-20 00:43:10 -0500

    WIP

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject nsanity "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject nsanity "0.0.1"
+  :description "A library to restore namespace sanity"
+  :url "https://github.com/royaldark/nsanity"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljfmt "0.5.7"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [medley "1.0.0"]
                  [rewrite-clj "0.6.0"]])
--- src/nsanity/core.clj
+++ src/nsanity/core.clj
@@ -1,6 +1,55 @@
-(ns nsanity.core)
+(ns nsanity.core
+  "nsanity - bringing sanity to your namespaces"
+  (:require [clojure.string :as str]
+            [medley.core :as medley]
+            [rewrite-clj.node :as n]
+            [rewrite-clj.zip :as z]))
 
-(defn foo
-  "I don't do a whole lot."
+(defn ^:private ->import-seq
   [x]
-  (println x "Hello, World!"))
+  (->> x
+       (iterate z/right)
+       (drop 1)
+       (take-while some?)))
+
+(defn ^:private ->ns-name
+  [zloc]
+  (if (= :vector (z/tag zloc))
+    (-> zloc z/down z/node n/string keyword)
+    (-> zloc z/node n/string keyword)))
+
+(defn ^:private sort-import-zlocs
+  [zlocs]
+  (->> zlocs
+       ->import-seq
+       (group-by ->ns-name)
+       (medley/map-kv (fn [ns-name zlocs]
+                        (assert (= 1 (count zlocs))
+                                (format "Multiple entries for NS %s: [%s]"
+                                        (name ns-name)
+                                        (str/join ", " (map z/string zlocs))))
+                        [ns-name (first zlocs)]))
+       (into (sorted-map))
+       vals))
+
+(defn ^:private reorg-import-zlocs
+  [orig sorted]
+  (reduce
+   (fn [zloc-old zloc-new]
+     (z/replace (z/right zloc-old)
+                (z/node zloc-new)))
+   orig
+   sorted))
+
+(defn nsanify
+  ([path]
+   (nsanify path nil))
+  ([path opts]
+   (let [data       (z/of-file path)
+         ns-form    (z/find-value data z/next 'ns)
+         ns-require (z/find-value ns-form z/next :require)]
+     (when ns-require
+       (let [sorted  (sort-import-zlocs ns-require)
+             reorg'd (reorg-import-zlocs ns-require sorted)]
+         (spit path
+               (n/string (z/root reorg'd))))))))
